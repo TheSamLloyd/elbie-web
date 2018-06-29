26b46a9d13d5b960f99f594fb64611a3a9823dc7
TheSamLloyd <samlloydj@gmail.com>
2018-06-29 17:38:56 -0400

    intial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TileComponent } from './tile/tile.component';
+import { HeaderComponent } from './header/header.component';
+import { TileDetailComponent } from './tile-detail/tile-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TileComponent,
+    HeaderComponent,
+    TileDetailComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/tile-detail/tile-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tile-detail',
+  templateUrl: './tile-detail.component.html',
+  styleUrls: ['./tile-detail.component.css']
+})
+export class TileDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
